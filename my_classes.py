b82038e849b90a1f2cc010c5bc97548e6fed25d2
Brandon Nguyen <brandonhnguyen@ucsb.edu>
2025-09-17 22:26:15 -0700

    Initial commit

---
+++ my_classes.py
@@ -0,0 +1,246 @@
+import pygame
+import random
+
+card_back = pygame.image.load("sprites\card-back.png")
+
+class Card:
+    dimensions = (36, 54)
+
+    def __init__(self, value, suit, location=None, slot=None):
+        self.value = value
+        self.suit = suit
+        self.int_value = self.card_value_to_int(value)
+
+        self.rect = None
+        self.is_face_up = True
+        self.is_draggable = True
+        self.location = location
+        self.image = pygame.image.load(f"sprites/{self.value}{self.suit}.png")
+
+        self.color = "red" if suit in ['H', 'D'] else "black"
+    
+    def render(self, screen):
+        if self.is_face_up:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
+        else:
+            screen.blit(card_back, self.rect)
+
+    def card_value_to_int(self, value):
+        if value == 'A':
+            return 1
+        elif value == 'J':
+            return 11
+        elif value == 'Q':
+            return 12
+        elif value == 'K':
+            return 13
+        else:
+            return int(value)
+    
+    @property
+    def location_index(self): # Is this function actually necessary? Why not just call location.cards.index(card)...
+        if self.location:
+            return self.location.cards.index(self)
+        return -1
+    
+    @property
+    def collision_rect(self):
+        if isinstance(self.location, Pile) and self.location.cards[-1] != self:
+            return pygame.Rect(self.rect.x, self.rect.y, Card.dimensions[0], 30)
+        else:
+            return pygame.Rect(self.rect.x, self.rect.y, *Card.dimensions)
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.rect = pygame.Rect(400, 50, *Card.dimensions)
+        self.image = pygame.image.load("sprites/card-back.png")
+        self.empty_image = pygame.image.load("sprites/card-placeholder.png")
+        suits = ['H', 'D', 'C', 'S']
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit, location='deck'))
+        random.shuffle(self.cards)
+
+    def draw_card(self):
+        if self.cards:
+            return self.cards.pop()
+        return None
+    
+    def draw_starting_piles(self):
+        piles = []
+
+        for i in range(7):
+            pile_cards = [self.draw_card() for _ in range(i + 1)]
+            pile = Pile(pile_cards, slot=i)
+            piles.append(pile)
+            for card in pile_cards:
+                card.location = pile
+
+        return piles
+
+    def render(self, screen):
+        if self.cards:
+            screen.blit(self.image, self.rect)
+        else:
+            screen.blit(self.empty_image, self.rect)
+
+class Pile:
+    def __init__(self, cards=None, slot=None):
+        self.cards = list(cards) if cards is not None else []
+        self.slot = slot
+        self.x = 50 + slot * 50
+        self.y = 150
+        self.card_offset = 30
+        self.empty_image = pygame.image.load("sprites/card-placeholder.png")
+        for card in self.cards[:-1]:
+            card.is_face_up = False
+            card.is_draggable = False
+        if self.cards:
+            self.cards[-1].is_face_up = True
+            self.cards[-1].is_draggable = True
+        for card in self.cards:
+            card.rect = pygame.Rect(self.x, self.y + self.cards.index(card) * self.card_offset, *Card.dimensions)
+            card.spot = self.slot
+        self.collision_rect = pygame.Rect(self.x, self.y, *Card.dimensions)
+
+    def flip(self):
+        if self.cards:
+            top_card = self.cards[-1]
+            top_card.is_face_up = True
+            top_card.is_draggable = True
+            return top_card
+        return None
+    
+    def render(self, screen):
+        if self.cards:
+            for card in self.cards:
+                card.render(screen)
+        else:  
+            rect = pygame.Rect(self.x, self.y, *Card.dimensions)
+            screen.blit(self.empty_image, rect)
+    
+    def validate_card_placement(self, card):
+        if not self.cards:
+            return card.value == 'K'  # Only Kings can be placed on empty piles
+        top_card = self.cards[-1]
+        if not top_card.is_face_up:
+            return False
+        # Check for alternating colors and descending order
+        if card.color != top_card.color and card.int_value == top_card.int_value - 1:
+            return True
+        return False
+    
+    @property
+    def collision_rect(self):
+        if self.cards:
+            top_card = self.cards[-1]
+            return top_card.rect
+        else:
+            return pygame.Rect(self.x, self.y, *Card.dimensions)
+        
+    @collision_rect.setter
+    def collision_rect(self, value):
+        self._collision_rect = value
+        
+    def append(self, card):
+        self.cards.append(card)
+        card.rect = pygame.Rect(self.x, self.y + (len(self.cards) - 1) * self.card_offset, *Card.dimensions)
+        return card
+    
+    def pop(self):
+        if self.cards:
+            card = self.cards.pop()
+            return card
+        return None
+
+
+class Foundation:
+    def __init__(self, suit=None, cards=None):
+        self.cards = list(cards) if cards is not None else []
+        self.suit = suit
+        foundation_offset = 50
+        self.x = 50 + ['H', 'D', 'C', 'S'].index(suit) * foundation_offset
+        self.y = 50
+        self.image = pygame.image.load(f"sprites/F{self.suit}.png")
+        self.collision_rect = pygame.Rect(self.x, self.y, *Card.dimensions)
+
+    def render(self, screen):
+        rect = pygame.Rect(self.x, self.y, *Card.dimensions)
+        if self.cards:
+            top_card = self.cards[-1]
+            top_card.rect = rect
+            top_card.render(screen)
+        else:
+            screen.blit(self.image, rect)
+
+    
+    def validate_card_placement(self, card):
+        if card.suit != self.suit:
+            return False
+        if not self.cards:
+            return card.value == 'A'  # Only Aces can be placed on empty foundations
+        top_card = self.cards[-1]
+        if card.int_value == top_card.int_value + 1:
+            return True
+        return False
+    
+    def append(self, card):
+        self.cards.append(card)
+        card.location = self
+        for cards in self.cards[:-1]:
+            cards.is_draggable = False
+        return card
+    
+    def pop(self):
+        if self.cards:
+            if len(self.cards) >= 2:
+                self.cards[-2].is_draggable = True
+            return self.cards.pop()
+        return None
+
+class Waste:
+    def __init__(self, cards=None):
+        self.cards = list(cards) if cards is not None else []
+        self.x = 400
+        self.y = 150
+
+    def append(self, card):
+        self.cards.append(card)
+        card.location = self
+        if self.cards:
+            self.cards[-1].is_draggable = True
+            for card in self.cards[:-1]:
+                card.is_draggable = False
+        return card
+
+    def loop(self, deck):
+        for card in self.cards:
+            card.location = deck
+        deck.cards.extend(reversed(self.cards))
+        self.cards.clear()
+    
+    def render(self, screen, n=3):
+        card_offset = 30
+        if self.cards:
+            for i, card in enumerate(self.cards[-n:]):
+                card.rect = pygame.Rect(self.x, self.y + i * card_offset, *Card.dimensions)
+                card.render(screen)
+
+    def pop(self):
+        if self.cards:
+            if len(self.cards) >= 2:
+                self.cards[-2].is_draggable = True
+            return self.cards.pop()
+        return None
+    
+def get_all_cards_on_screen(piles, waste, foundations):
+    cards_on_screen = []
+    for pile in piles:
+        cards_on_screen.extend(pile.cards)
+    cards_on_screen.extend(waste.cards)
+    for foundation in foundations:
+        cards_on_screen.extend(foundation.cards)
+    return cards_on_screen
---
+++ solitaire.py
@@ -0,0 +1,150 @@
+import pygame
+from my_classes import Card, Deck, Pile, Foundation, Waste, get_all_cards_on_screen
+
+pygame.init()
+pygame.display.set_caption('Solitaire')
+icon = pygame.image.load('sprites/KH.png')
+pygame.display.set_icon(icon)
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
+running = True
+
+cards_being_dragged = []
+card_mouse_offset = 0
+drag_card_original_pos = None
+draw_clicked = False
+
+deck = Deck()
+piles = deck.draw_starting_piles()
+waste = Waste()
+foundations = [Foundation(suit=suit) for suit in ['H', 'D', 'C', 'S']]
+
+pygame.mixer.init()
+pygame.mixer.music.load("music/luigi.mp3")
+pygame.mixer.music.play(-1)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("darkgreen")
+    pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+    if (deck.rect.collidepoint(pygame.mouse.get_pos())):
+        pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+
+    keystate = pygame.key.get_pressed()
+
+    # Handle drawing
+    # On click on deck or 'SPACE' press,
+    if ((deck.rect.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]) or (keystate[pygame.K_SPACE])) and not draw_clicked and not cards_being_dragged:
+        draw_clicked = True
+
+        if deck.cards:
+            card_drawn = deck.draw_card()
+            waste.append(card_drawn)
+            if card_drawn:
+                pygame.time.delay(100)
+        else:
+            waste.loop(deck)
+
+    # Debounce for drawing
+    if not (pygame.mouse.get_pressed()[0] or keystate[pygame.K_SPACE]):
+        draw_clicked = False
+        
+    # Render all game elements
+    for pile in piles:
+        pile.render(screen)
+
+    for foundation in foundations:
+        foundation.render(screen)
+
+    if drag_card_original_pos and isinstance(drag_card_original_pos, Waste):
+        waste.render(screen, n=2)
+    else:
+        waste.render(screen, n=3)
+
+    deck.render(screen)
+
+    # Check for clicks on cards
+    # BUG: If you're holding MB1 and drag your cursor over a card it'll scoop it up.
+    for card in get_all_cards_on_screen(piles, waste, foundations):
+        if card.collision_rect.collidepoint(pygame.mouse.get_pos()) and card.is_draggable:
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            if pygame.mouse.get_pressed()[0] and not cards_being_dragged:
+                cards_being_dragged.append(card)
+                drag_card_original_pos = card.location
+
+                # Handle stacks in piles
+                if isinstance(card.location, Pile):
+                    pile = card.location
+                    cards_being_dragged = pile.cards[card.location_index:]
+
+                # Remove card from original location
+                for card in cards_being_dragged:
+                    card.location.pop()
+                    card.location = None
+                
+                # Keep track of offset for dragging
+                card_mouse_offset = cards_being_dragged[0].rect.topleft - pygame.Vector2(pygame.mouse.get_pos())    
+
+    # On MB1 release,
+    # BUG: Moving a card to a pile from the waste feels weird because the waste shifts back after snapping to pile
+    if cards_being_dragged and not pygame.mouse.get_pressed()[0]:
+        cards = cards_being_dragged
+        top_card = cards[0]
+        cards_being_dragged = []
+        valid_placement = False
+
+        # Check if valid placement for foundation or pile, if so place the card there!
+        for pile in piles:
+            if pile.collision_rect.colliderect(top_card.rect):
+                if pile.validate_card_placement(top_card):
+                    valid_placement = True
+                    for card in cards:
+                        pile.append(card)
+                        card.location = pile
+                    break
+
+        for foundation in foundations:
+            if foundation.collision_rect.colliderect(top_card.rect) and foundation.validate_card_placement(top_card) and len(cards) == 1:
+                foundation.append(top_card)
+                top_card.location = foundation
+                valid_placement = True
+                break
+
+        # Return card if placement not valid
+        if not valid_placement:
+            for card in cards:
+                drag_card_original_pos.append(card)
+                card.location = drag_card_original_pos
+        
+        # If card was originally in a pile, reveal the next card
+        if drag_card_original_pos and isinstance(drag_card_original_pos, Pile):
+            drag_card_original_pos.flip()
+
+        drag_card_original_pos = None
+
+        pygame.time.delay(100)
+    
+    # Handle cards being dragged around
+    if cards_being_dragged:
+        stack_offset = 30
+        cards_being_dragged[0].rect.topleft = pygame.mouse.get_pos()[0] + card_mouse_offset.x, pygame.mouse.get_pos()[1] + card_mouse_offset.y
+        for i, card in enumerate(cards_being_dragged[1:], start=1):
+            card.rect.topleft = (cards_being_dragged[0].rect.x, cards_being_dragged[0].rect.y + i * stack_offset)
+
+        for card in cards_being_dragged:
+            card.render(screen)
+    
+    # Game win detection
+    foundation_top_cards = [foundation.cards[-1].value for foundation in foundations if foundation.cards]
+    if foundation_top_cards == ['K', 'K', 'K', 'K']:
+        print('You won!')
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
